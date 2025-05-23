a609dc68a182206ea724124750aae96c6540a4ab
Karla Martinez <karla.m5161@gmail.com>
2025-05-22 22:28:37 -0700

    Slideshow works with css change no longer using animation-scroll from F-ing tailwind

--- src/components/landing/Slideshow.tsx
+++ src/components/landing/Slideshow.tsx
@@ -1,7 +1,7 @@
 //import { useTranslation } from "react-i18next";
+import "./Slideshow.css";
 
 import Kiwi1 from "../../assets/slideshow/kiwi/Kiwi1.jpg";
-//import Kiwi1 from "@/assets/slideshow/kiwi/Kiwi1.jpg";
 import Kiwi2 from "../../assets/slideshow/kiwi/Kiwi2.jpg";
 import Kiwi3 from "../../assets/slideshow/kiwi/Kiwi3.jpg";
 import Kiwi4 from "../../assets/slideshow/kiwi/Kiwi4.jpg";
@@ -14,7 +14,7 @@ const Slideshow = () => {
 
   return (
     <div className="overflow-hidden bg-pink-200 min-h-[200px] border-b-3 border-purple-300 items-center justify-center mt-10 p-6 rounded-4xl mx-auto md:max-w-6xl">
-      <div className="flex animate-scroll gap-8 w-max">
+      <div className="scroll-animation">
         {kiwiPhotos.map((img, index) => (
           <img
             key={index}
